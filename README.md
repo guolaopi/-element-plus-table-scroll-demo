c51a5955783768a9b493c50ce19c734a42154b90
guolaopi <guolaopi@gmail.com>
2022-08-04 17:18:28 +0800

    add code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# -element-plus-table-scroll-demo
-Example of  element-plus-table-scroll.
+# element-plus-table-scroll-demo
+Example of element-plus-table-scroll.
+
+here is the link: [element-plus-table-scroll-demo](https://guolaopi.github.io/element-plus-table-scroll-demo)
---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+    <h2>Try to scroll the table.</h2>
+    <el-table
+        height="200"
+        key="id"
+        border
+        v-scrollLoad="scrollLoad"
+        :data="data"
+    >
+        <el-table-column
+            v-for="item in columns"
+            :key="item.prop"
+            :prop="item.prop"
+            :label="item.label"
+        />
+    </el-table>
+</template>
+
+<script setup>
+import { vScrollLoad } from "element-plus-table-scroll";
+import { ElMessage } from "element-plus";
+import { ref } from "vue";
+
+const columns = ref([
+    { prop: "id", label: "ID" },
+    { prop: "name", label: "Name" },
+    { prop: "sex", label: "Sex" },
+]);
+
+const data = ref([
+    { id: 1, name: "UserA", sex: "Male" },
+    { id: 2, name: "UserB", sex: "Female" },
+    { id: 3, name: "UserC", sex: "Male" },
+    { id: 4, name: "UserD", sex: "Male" },
+    { id: 5, name: "UserE", sex: "Female" },
+    { id: 6, name: "UserF", sex: "Male" },
+    { id: 7, name: "UserG", sex: "Male" },
+    { id: 8, name: "UserH", sex: "Female" },
+    { id: 9, name: "UserI", sex: "Female" },
+]);
+
+function scrollLoad(e) {
+    if (
+        e.target.offsetHeight + e.target.scrollTop + 50 >=
+        e.target.scrollHeight
+    ) {
+        console.log(1);
+        data.value = data.value.concat(data.value);
+    }
+}
+</script> 
